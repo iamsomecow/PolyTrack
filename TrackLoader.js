ef1ba12227effe75b9e8a4ceff806faaa3de1847
iamsomecow <145311683+iamsomecow@users.noreply.github.com>
2025-06-19 11:04:42 +1000

    done

---
+++ TrackLoader.js
@@ -0,0 +1,10 @@
+const tracks = [
+    "/PolyTrack/tracks/track_8.track"
+]
+function load(func, v){
+    let out = [];
+    for (let index = 0; index < tracks.length; index++) {
+        out[index] = func(tracks[index], v);  
+    }
+    return out;
+}
